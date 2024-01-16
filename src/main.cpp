fe42baae6a57d07e4bc105bc0bdc878979d334e6
Trim Bresilla <trim.bresilla@gmail.com>
2024-01-16 01:29:12 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <fmt/format.h>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
