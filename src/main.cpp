36b0b37fa9524f19b475eb765c1fe3001882b047
Trim Bresilla <trim.bresilla@gmail.com>
2024-01-16 02:12:38 +0100

    feat: add `spdlog` support and improve GLFW integration
    - Modify `compile_commands.json`:
      - Add additional include directories for compilation
    - Modify `src/main.cpp`:
      - Add `spdlog` header inclusion
      - Add GLFW error callback function
      - Add GLFW key callback function
      - Modify main function to initialize GLFW and create a window
    - Modify `xmake.lua`:
      - Add `spdlog` package requirement

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <fmt/format.h>
+#include <spdlog/spdlog.h>
 
-using namespace std;
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl;
-    return 0;
+
+void glfw_error_callback(int error, const char* description){
+    spdlog::error("GLFW error: {}", description);
+}
+
+void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+    // keycode in ASCII
+    spdlog::info("Key pressed: {}", (char)key);
+}
+
+int main(){
+    if(!glfwInit()){
+        spdlog::error("Failed to initialize GLFW");
+        return -1;
+    }
+    spdlog::info("GLFW initialized");
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    glfwSetErrorCallback(glfw_error_callback);
+    glfwSetKeyCallback(window, glfw_key_callback);
+
+    if(!window){
+        spdlog::error("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+    spdlog::info("GLFW window created");
+
+    while(!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
 }
