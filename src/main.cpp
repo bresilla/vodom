80cf3324c5e61b453c341cd996c02507177c5420
Trim Bresilla <trim.bresilla@gmail.com>
2024-01-19 17:13:26 +0100

    refactor: refactor project file structure and configurations
    - Added a new file `.github/workflows/release.yaml`
    - Modified the file `.gitignore` by adding a new line with `.cache`
    - Modified the file `.vscode/c_cpp_properties.json` by removing the line `"cStandard": "c17"`
    - Deleted the file `.vscode/compile_commands.json`
    - Renamed the file `a/.vscode/c_cpp_properties.json` to `b/.vscode/c_cpp_properties.json`
    - Renamed the file `a/.vscode/compile_commands.json` to `b/.vscode/compile_commands.json`
    - Renamed the file `a/compile_commands.json` to `b/compile_commands.json`
    - Modified the file `justfile` by adding a new line with `do type`
    - Modified the file `src/main.cpp` by removing some includes and code
    - Modified the file `xmake.lua` by removing some lines

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 #include "utils.hpp"
 
-#include <iostream>
-#include <filesystem>
-#include <fstream>
-
 #include <opencv2/opencv.hpp>
 #include <Eigen/Dense>
 #include <fmt/format.h>
@@ -11,36 +7,11 @@
 
 
 
-namespace ei = Eigen;
+// namespace ei = Eigen;
 namespace spd = spdlog;
 
-
-void featureDetection(cv::Mat img_1, std::vector<cv::Point2f>& points1){ 
-    std::vector<cv::KeyPoint> keypoints_1;
-    int fast_threshold = 20;
-    bool nonmaxSuppression = true;
-    cv::FAST(img_1, keypoints_1, fast_threshold, nonmaxSuppression);
-    cv::KeyPoint::convert(keypoints_1, points1, std::vector<int>());
-}
-
-void featureTracking(cv::Mat img_1, cv::Mat img_2, std::vector<cv::Point2f>& points1, std::vector<cv::Point2f>& points2, std::vector<uchar>& status){ 
-    cv::Size winSize = cv::Size(21,21);																								
-    cv::TermCriteria termcrit = cv::TermCriteria(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 30, 0.01);
-    cv::calcOpticalFlowPyrLK(img_1, img_2, points1, points2, status, err, winSize, 3, termcrit, 0, 0.001);
-    int indexCorrection = 0;
-    for( int i=0; i<status.size(); i++){ 
-            cv::Point2f pt = points2.at(i- indexCorrection);
-            if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0)){
-                if((pt.x<0)||(pt.y<0)){ status.at(i) = 0; }
-                points1.erase (points1.begin() + i - indexCorrection);
-                points2.erase (points2.begin() + i - indexCorrection);
-                indexCorrection++;
-            }
-    }
-}
-
 int main(){
-    utl::ImageLoader color = utl::ImageLoader("/doc/code/LEARN/vodom/dataset/rgbd_dataset_freiburg3_long_office_household/rgb/");
+    utl::ImageLoader color = utl::ImageLoader("/doc/code/PROJECTS/vodom/dataset/rgb/");
     for(const auto& img : color){
         cv::imshow("img", img);
         spd::info("{}", color.info());
--- xmake.lua
+++ xmake.lua
@@ -1,30 +1,18 @@
 add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
+-- add_requires("muslcc")
+-- set_toolchains("@muslcc")
 
 add_requires("opencv", {system = true})
-add_requires("eigen", {system = true})
-add_requires("fmt", {system = true})
-add_requires("spdlog", {system = true})
-
--- target("inference")
---     set_kind("static")
---     add_includedirs("include")
---     add_files("src/*.cpp")
---     add_packages("opencv")
-
--- target("standalone")
---     set_kind("binary")
---     add_files("src/standalone.cpp")
---     add_packages("opencv")
---     add_includedirs("include")
-
+add_requires("eigen", {system = false})
+add_requires("fmt", {system = false})
+add_requires("spdlog", {system = false}) 
+ 
 target("run")
-    -- add_deps("inference")
     set_kind("binary")
     add_includedirs("include")
     add_files("src/*.cpp")
-    -- add_files("src/inference.cpp")
     add_packages("opencv")
     add_packages("eigen")
     add_packages("fmt")
